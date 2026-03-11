8b3de24c056b6b828693b122ba112d20020a0aa6
roniahamed <mdroniahamed56@gmail.com>
2026-03-12 00:36:56 +0600

    Add UserSerializer to handle user data and integrate Firebase token serialization

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -1,4 +1,8 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class FirebaseTokenSerializer(serializers.Serializer):
     id_token = serializers.CharField(required=True, help_text="Firebase ID Token")
@@ -7,3 +11,10 @@ class FirebaseTokenSerializer(serializers.Serializer):
     last_name = serializers.CharField(required=False, allow_blank=True, max_length=150)
     user_type = serializers.CharField(required=False, default='patnabor')
     agree_to_terms_and_conditions = serializers.BooleanField(required=False, default=False)
+    
+    
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'phone', 'first_name', 'last_name', 'user_type', 'is_verified', 'is_active', 'is_staff', 'is_superuser', 'created_at', 'agree_to_terms_and_conditions', 'is_patpal', 'is_online', 'last_active', 'firebase_uid', 'username', 'updated_at']
+        read_only_fields = ['id', 'is_verified', 'is_active', 'is_staff', 'is_superuser', 'created_at', 'email', 'phone', 'updated_at', 'firebase_uid', 'username']
