bcde9e18ce86ccb028daac9f467fb4b220eda2db
roniahamed <mdroniahamed56@gmail.com>
2026-03-12 00:37:05 +0600

    Add UserDetailView URL pattern for user data retrieval

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import FirebaseLoginView
+from .views import FirebaseLoginView, UserDetailView
 
 
 
 urlpatterns = [
     path('login/firebase/', FirebaseLoginView.as_view(), name='firebase-login'),
+    path('user/', UserDetailView.as_view(), name='user-detail'),
 ]
