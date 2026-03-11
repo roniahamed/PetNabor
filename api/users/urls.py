955ae868eac4d02a0033a19badda53a307bb4072
roniahamed <mdroniahamed56@gmail.com>
2026-03-12 02:11:47 +0600

    Add NotificationSettingsView URL pattern for user notification settings

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import FirebaseLoginView, UserDetailView
+from .views import FirebaseLoginView, UserDetailView, NotificationSettingsView
 
 
 
 urlpatterns = [
     path('login/firebase/', FirebaseLoginView.as_view(), name='firebase-login'),
     path('user/', UserDetailView.as_view(), name='user-detail'),
+    path('user/notification-settings/', NotificationSettingsView.as_view(), name='notification-settings'),
+    
 ]
