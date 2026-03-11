70bea6f00834a0487a814ad1a73fceb2ede2f0c0
roniahamed <mdroniahamed56@gmail.com>
2026-03-12 02:40:47 +0600

    Add signals for user notification settings management

--- api/notifications/apps.py
+++ api/notifications/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class NotificationsConfig(AppConfig):
     name = 'api.notifications'
+    
+    def ready(self):
+        import api.notifications.signals  # noqa: F401
+
---
+++ api/notifications/signals.py
@@ -0,0 +1,17 @@
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from api.notifications.models import NotificationSettings
+from django.db.models.signals import post_save
+
+
+User = get_user_model()
+
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_notification_settings(sender, instance, created, **kwargs):
+    if created:
+        NotificationSettings.objects.create(user=instance)
+    else:
+        if hasattr(instance, 'notification_settings'):
+            instance.notification_settings.save()
