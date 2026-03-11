0d32890879c7ab19377da8d90983c28b703c4d84
roniahamed <mdroniahamed56@gmail.com>
2026-03-12 01:53:18 +0600

    Add user profile and notification settings models, media configuration, and signal handlers

--- api/users/apps.py
+++ api/users/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'api.users'
+
+    def ready(self):
+        import api.users.signals  # noqa: F401
--- config/settings.py
+++ config/settings.py
@@ -141,6 +141,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# Media 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # REST Framework configuration
 
 REST_FRAMEWORK = {
