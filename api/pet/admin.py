6b558450cf28a0501ac2ece4dba4cd12db12ab5f
roniahamed <mdroniahamed56@gmail.com>
2026-03-12 02:42:23 +0600

    Add PetProfile model with fields and admin registration

--- api/pet/admin.py
+++ api/pet/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import PetProfile
 
-# Register your models here.
+    
+@admin.register(PetProfile)
+class PetProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'pet_name', 'pet_type', 'size', 'weight', 'weight_type', 'date_of_birth')
+    search_fields = ('user__email', 'user__username', 'pet_name')
+    ordering = ('user__id',)
+    
