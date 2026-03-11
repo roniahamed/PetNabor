bfe91af605a8b9808948e7f0114c6d39b2e7ca26
roniahamed <mdroniahamed56@gmail.com>
2026-03-12 02:31:20 +0600

    Remove NotificationSettingsView URL pattern from user URLs

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import FirebaseLoginView, UserDetailView, NotificationSettingsView
-
+from .views import FirebaseLoginView, UserDetailView
 
 
 urlpatterns = [
     path('login/firebase/', FirebaseLoginView.as_view(), name='firebase-login'),
     path('user/', UserDetailView.as_view(), name='user-detail'),
-    path('user/notification-settings/', NotificationSettingsView.as_view(), name='notification-settings'),
     
 ]
