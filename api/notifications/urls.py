a2803ee218704598b845d6ce9391f5a12ebeb9e3
roniahamed <mdroniahamed56@gmail.com>
2026-03-13 06:47:43 +0600

    Refactor: Adjust notification API routing and apply code style improvements to views.

--- api/notifications/urls.py
+++ api/notifications/urls.py
@@ -3,11 +3,15 @@ from rest_framework.routers import DefaultRouter
 from .views import NotificationSettingsView, FCMDeviceView, NotificationViewSet
 
 router = DefaultRouter()
-router.register(r'notifications', NotificationViewSet, basename='notifications')
+router.register(r"", NotificationViewSet, basename="notifications")
 
 
 urlpatterns = [
-    path('notification-settings/', NotificationSettingsView.as_view(), name='notification-settings'),
-    path('fcm-devices/', FCMDeviceView.as_view(), name='fcm-devices'),
-    path('notifications/', include(router.urls))
+    path("notifications/", include(router.urls)),
+    path(
+        "notification-settings/",
+        NotificationSettingsView.as_view(),
+        name="notification-settings",
+    ),
+    path("fcm-devices/", FCMDeviceView.as_view(), name="fcm-devices"),
 ]
