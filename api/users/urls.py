53fca1b396c0378dccfd420a4c54c6d771168d59
roniahamed <mdroniahamed56@gmail.com>
2026-03-14 23:23:30 +0600

    feat: Add token refresh endpoint and update Redis host port mapping in Docker Compose.

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 from .views import FirebaseLoginView, UserDetailView, ProfileDetailView
 
 
@@ -6,4 +9,5 @@ urlpatterns = [
     path("login/firebase/", FirebaseLoginView.as_view(), name="firebase-login"),
     path("user/", UserDetailView.as_view(), name="user-detail"),
     path("profile/", ProfileDetailView.as_view(), name="profile-detail"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
 ]
