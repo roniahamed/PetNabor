1135ce43518a4deea14089974ed8eee22ddefbb0
roniahamed <mdroniahamed56@gmail.com>
2026-03-15 00:52:19 +0600

    feat: Add initial friends application structure and implement PetProfileViewSet for pet profiles.

--- api/pet/views.py
+++ api/pet/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import PetProfile
+from .serializers import PetProfileSerializer
 
-# Create your views here.
+class PetProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = PetProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return PetProfile.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
