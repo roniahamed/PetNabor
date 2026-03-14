93059e2f2f71a146e26b67f266ca36a1bde05bc0
roniahamed <mdroniahamed56@gmail.com>
2026-03-15 00:52:32 +0600

    feat: Scaffold new `friends` app and add `pets` API URL.

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('users/', include('api.users.urls')),
     path('notifications/', include('api.notifications.urls')),
-    
+    path('pets/', include('api.pet.urls')),  
 ]
