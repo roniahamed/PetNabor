a09ee1b88b54f58731b42527a03bbaecc90c43a2
roniahamed <mdroniahamed56@gmail.com>
2026-03-16 01:07:18 +0600

    feat: Register friend request, friendship, and user block models in Django admin and update gitignore to exclude UI design files.

--- api/friends/admin.py
+++ api/friends/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import FriendRequest, Friendship, UserBlock
 
-# Register your models here.
+
+@admin.register(FriendRequest)
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_display = ("sender", "receiver", "status", "created_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("sender__username", "receiver__username")
+
+
+@admin.register(Friendship)
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ("user1", "user2", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user1__username", "user2__username")
+
+
+@admin.register(UserBlock)
+class UserBlockAdmin(admin.ModelAdmin):
+    list_display = ("blocker", "blocked_user", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("blocker__username", "blocked_user__username")
