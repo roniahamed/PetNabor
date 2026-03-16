bdf7028af0aee275a6ac6fd69cbc18a135f78287
roniahamed <mdroniahamed56@gmail.com>
2026-03-17 00:52:42 +0600

    feat: Introduce Nginx as a reverse proxy and static/media file server, updating Docker Compose configuration and adding static file collection.

--- config/settings.py
+++ config/settings.py
@@ -182,6 +182,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
 echo "Applying database migrations..."
 python manage.py migrate
 
