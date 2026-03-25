a1732922d1cc4c42156fdbc6277eb9c190bb0eb3
roniahamed <mdroniahamed56@gmail.com>
2026-03-26 04:05:51 +0600

    feat: Update email configuration to use environment variables for SMTP settings

--- config/settings.py
+++ config/settings.py
@@ -264,12 +264,12 @@ TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER", "")
 # ──────────────────────────────────────────────
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv("EMAIL_HOST", "")
+EMAIL_PORT = os.getenv("EMAIL_PORT", "")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
-DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER", "noreply@patnabor.com")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER", "")
 
 
 # ──────────────────────────────────────────────
