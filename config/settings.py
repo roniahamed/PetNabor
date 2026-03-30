57c17b839680a52d1fd9dc901399a1129fc3c01d
roniahamed <mdroniahamed56@gmail.com>
2026-03-31 05:45:41 +0600

    feat: update Nginx configuration for backend.petnabor.com and add TLS support

--- config/settings.py
+++ config/settings.py
@@ -25,11 +25,19 @@ ALLOWED_HOSTS = (
     else []
 )
 
+ALLOWED_HOSTS += ["backend.petnabor.com", "172.252.13.85", "localhost", "127.0.0.1"]
+
 CSRF_TRUSTED_ORIGINS = (
     os.getenv("CSRF_TRUSTED_ORIGINS").split(",")
     if os.getenv("CSRF_TRUSTED_ORIGINS")
     else []
-) + ["http://localhost:8001", "http://127.0.0.1:8001"]
+) + [
+    "http://localhost:8002",
+    "http://127.0.0.1:8002",
+    "https://backend.petnabor.com",
+    "http://172.252.13.85",
+    "https://172.252.13.85",
+]
 
 INSTALLED_APPS = [
     "daphne",
