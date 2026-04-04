cef870f1b7f750eae79b65fa39604c72da1b4798
roniahamed <mdroniahamed56@gmail.com>
2026-04-05 02:51:48 +0600

    fix(admin): Patch Django unfold to safely query UUID fields

--- api/users/apps.py
+++ api/users/apps.py
@@ -6,3 +6,34 @@ class UsersConfig(AppConfig):
 
     def ready(self):
         import api.users.signals  # noqa: F401
+
+        # Globally patch Unfold Admin to prevent DataErrors when searching UUIDs
+        import uuid
+        from unfold.admin import ModelAdmin
+
+        original_get_search_results = ModelAdmin.get_search_results
+
+        def custom_get_search_results(self, request, queryset, search_term):
+            old_search_fields = self.search_fields
+            if search_term and old_search_fields:
+                is_uuid = False
+                try:
+                    uuid.UUID(search_term)
+                    is_uuid = True
+                except ValueError:
+                    pass
+
+                if not is_uuid:
+                    # Strip 'id' / '=id' / 'model__id' fields to prevent crash evaluating non-strings
+                    self.search_fields = [
+                        f for f in old_search_fields 
+                        if not f.endswith('id') and not f.endswith('__id')
+                    ]
+            
+            try:
+                res = original_get_search_results(self, request, queryset, search_term)
+            finally:
+                self.search_fields = old_search_fields
+            return res
+            
+        ModelAdmin.get_search_results = custom_get_search_results
