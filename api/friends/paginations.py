8d9f2a55629aa70de9bc71a46e8927d14610904c
roniahamed <mdroniahamed56@gmail.com>
2026-04-12 01:07:09 +0600

    chore: increase default page_size to 100 in StandardResultsSetPagination

--- api/friends/paginations.py
+++ api/friends/paginations.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 100
 
